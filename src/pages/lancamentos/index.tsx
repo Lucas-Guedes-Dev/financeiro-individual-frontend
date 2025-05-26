ec49e7d527ee299bb0f991822f9b42d4577012aa
lucas <lucas_guidolinguedes@outlook.com>
2025-05-26 20:45:31 -0300

    subindo exemplo

--- src/pages/lancamentos/index.tsx
+++ src/pages/lancamentos/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useEffect, useState } from "react";
+import { Container, Description, Div1, Div2 } from "./style";
 
 
 const Lacamentos: React.FC = () => {
+    const [text, setText] = useState<string>('');
+
+    useEffect(() => {
+        console.log(text)
+    }, [text])
+
     return (
         <Container>
-            lancamentos
+            <Div1>
+                <Description value={text} onChange={(event) => setText(event?.target.value)} />
+            </Div1>
+            <Div2>
+
+            </Div2>
         </Container>
     )
 }
