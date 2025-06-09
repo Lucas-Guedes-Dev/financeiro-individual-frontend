f1c0fda907d689825ad8efc5fa5516f20abaeb99
HenriqueMason <156488650+HenriqueMason@users.noreply.github.com>
2025-06-08 21:43:42 -0300

    Tela de Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,50 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useState } from "react";
+import {
+  Container,
+  Titulo,
+  Form,
+  Input,
+  Button,
+  Button2,
+  LogoImg,
+} from "./style";
+import Logo from "../../assets/Logo.jpeg";
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    return (
-        <Container>
-            Login
-        </Container>
-    )
-}
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Senha:", senha);
+    // Aqui você conecta com sua API, se quiser
+  };
+
+  return (
+    <Container>
+      <LogoImg src={Logo} alt="Logo" />
+      <Titulo>Faça seu login</Titulo>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          required
+        />
+        <Button2 type="submit">Cadastrar</Button2>
+        <Button type="submit">Entrar</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default Login;
--- src/pages/login/style.tsx
+++ src/pages/login/style.tsx
@@ -1,10 +1,60 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: red;
-    width: 100%;
-    height: 100vh;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Titulo = styled.h2`
+  margin-bottom: 20px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  gap: 16px;
+`;
+
+export const Input = styled.input`
+  padding: 12px;
+  font-size: 16px;
+`;
+
+export const Button = styled.button`
+  padding: 12px;
+  font-size: 16px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export const Button2 = styled.button`
+  padding: 12px;
+  font-size: 16px;
+  background-color: rgb(55, 0, 255);
+  color: white;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export const LogoImg = styled.img`
+  width: 200px;
+  height: auto;
+  margin-bottom: 10px;
+  margin-top: 0px;
+`;
